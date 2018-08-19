39a3d9f78aaf1613e43a612b8f03186d57c1a64d
Shriney Malekar <shrineymalekar@gmail.com>
2018-08-19 20:26:24 +0530

    Home page created

--- App.js
+++ App.js
@@ -1,23 +1,77 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          style={styles.button}
+          onPress={() => this.props.navigation.navigate('AddProduct')}
+          title="Add Product"
+          backgroundColor= 'powderblue'
+          accessibilityLabel="Add Product"
+        />
+        <Button
+          style={styles.button}
+          onPress={() => this.props.navigation.navigate('AddProduct')}
+          title="Sell Product"
+          backgroundColor= 'skyblue'
+          accessibilityLabel="Sell Product"
+        />
+        <Button
+          style={styles.button}
+          onPress={() => this.props.navigation.navigate('AddProduct')}
+          title="View Report"
+          backgroundColor= 'steelblue'
+          accessibilityLabel="View Report"
+        />
       </View>
     );
+  }
+}
+
+class FormScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const Inventory = createStackNavigator(
+  {
+    Home: HomeScreen,
+    AddProduct: FormScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  onPressViewReport = () => {
+    return "hello";
+  }
+  render() {
+    return <Inventory />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+  },
+  button: {
+    width: 50, 
+    height: 50,
+    padding: '1em'
   },
 });
